c339ee67445a5b77bec2f8cec63a77af72369caf
Md Nabiul Bashar <nabiultasbed@gmail.com>
2023-02-17 19:04:41 +0600

    product-reducer added

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
-import productReducer from "./reducers/productReducer";
 import { composeWithDevTools } from 'redux-devtools-extension';
+import productReducer from "./reducers/productReducer";
 const store = createStore(productReducer, composeWithDevTools());
 
 export default store;
